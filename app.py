9bfd3d741739960ceb8a04a011c4c2080b31e94c
Deepak C Shetty <dpkshetty@gmail.com>
2022-02-11 16:39:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello, World ! - Pyflask Demo - VERSION 2.0 - testing on rpsene cluster </h1>'
+    return '<h1>Hello, World ! - Pyflask Demo - demo11 </h1>'
 
 @app.route('/version')
 def get_version():
